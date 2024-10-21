442e8842f07cc5f0dffe97506912a2e53ae846fb
tagir2211 <tagir2211@gmail.com>
2024-10-21 21:45:43 +0300

    module_12_1.py

--- module_12_1.py
+++ module_12_1.py
@@ -1,5 +1,7 @@
 import unittest
+
 import runner
+
 
 class RunnerTest(unittest.TestCase):
     def test_walk(self):
@@ -11,6 +13,7 @@ class RunnerTest(unittest.TestCase):
         for _ in range(1, 11):
             t_walk.walk()
         self.assertEqual(t_walk.distance, 50)
+
     def test_run(self):
         '''
         Test class Runner, functoin run
@@ -19,7 +22,8 @@ class RunnerTest(unittest.TestCase):
         t_run = runner.Runner('Test')
         for _ in range(1, 11):
             t_run.run()
-        self.assertEqual(t_run.distance,100)
+        self.assertEqual(t_run.distance, 100)
+
 
 if __name__ == '__main__':
     unittest.main()
