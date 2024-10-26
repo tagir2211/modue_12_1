162519fb64ad9f5f419974488a6bf962e7ffa986
tagir2211 <tagir2211@gmail.com>
2024-10-26 17:49:25 +0300

    Add files via upload

--- module_12_1.py
+++ module_12_1.py
@@ -6,7 +6,7 @@ import runner
 class RunnerTest(unittest.TestCase):
     def test_walk(self):
         '''
-        Test class Runner, functoin walc
+        Test class Runner, functoin walk
         :return:
         '''
         t_walk = runner.Runner('Test')
@@ -23,6 +23,14 @@ class RunnerTest(unittest.TestCase):
         for _ in range(1, 11):
             t_run.run()
         self.assertEqual(t_run.distance, 100)
+
+    def test_challenge(self):
+        t_walk = runner.Runner('Wallker')
+        t_run = runner.Runner('Runner')
+        for _ in range(1, 11):
+            t_walk.walk()
+            t_run.run()
+        self.assertNotEqual(t_run.distance, t_walk.distance)
 
 
 if __name__ == '__main__':
